eaa86c4adf10acd729e7c56571ae2171e8f9cc24
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-12 20:21:54 +0200

    add logo

--- client/simple-dapp/src/components/Navbar.jsx
+++ client/simple-dapp/src/components/Navbar.jsx
@@ -1,6 +1,13 @@
+ import { HiMenuAlt4 } from 'react-icons/hi';
+ import{ AiOutlineClose } from 'react-icons/ai'
+ import logo from '../../img/logo.png'
 const Navbar = () => {
    return(
-      <h1>Navbar</h1>
+      <nav className='w-ful flex md:justify-centre justify-between items-centre p-4  '>
+         <div className='md:flex-[0.5] flex-initial justify-centre items-centre'>
+            <img src={logo} alt="logo" className='w-32 cursor-pointer' />
+         </div>
+      </nav>
    );
   
 }
