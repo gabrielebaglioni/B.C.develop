c84a235c916a3de2f997c812085db55973433c06
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-12 17:36:53 +0200

    first commit

---
+++ client/simple-dapp/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import './index.css'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+       <h1 class="text-3xl font-bold underline">
+         Hello world!
+       </h1>
+    </div>
+  )
+}
+
+export default App
