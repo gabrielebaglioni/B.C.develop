187e2943c0ae17476bba7a78a98758bd6c540c77
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-13 19:45:50 +0200

    add working logic to store and do the transaction

--- smart_contract/contracts/transactions.sol
+++ smart_contract/contracts/transactions.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 contract transactions{
-   uint256 transactionsCounter;
+   uint256 transactionsCount;
 
    event Tranfer( address from, address reciver, uint amount, string message, uint256 timestamp, string keyword );
    struct TranferStruct{
@@ -15,13 +15,18 @@ contract transactions{
    TranferStruct[] transactions;
    //get an 'object' of all the dipendecy that i need for store and register all the transactions
 
-   function addToBlockchain() public {
+   function addToBlockchain( address payable reciver, uint amount, string memory message, string memory keyword ) public {
+      transactionsCount += 1;
+      transactions.push(TranferStruct(msg.sender, reciver, amount, message, block.timestamp, keyword));
 
+      emit Tranfer(msg.sender, reciver, amount, message, block.timestamp, keyword);
    }
    function getAllTransactions() public view returns (TranferStruct[] memory) {
 // retur transactions
+      return transactions;
    }
-   function getTransactionsCount() public view returns (uint256){
-// retourns transactions count
+   function getTransactsCount() public view returns (uint256){
+// retourns transactionions count
+      return transactionsCount;
    }
 }
