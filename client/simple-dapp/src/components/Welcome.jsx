b63905115dc1f2c0c69ef0e734f370abd23fe708
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-13 16:00:53 +0200

    add input form

--- client/simple-dapp/src/components/Welcome.jsx
+++ client/simple-dapp/src/components/Welcome.jsx
@@ -5,8 +5,22 @@ import { Loader } from './'
 const Welcome = () => { 
 
    const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
-   const connectWallet = () =>{
+   const Imput = ({placeholder, type, name,  value, handleChange}) => (
+      <input
+       type={type} 
+       placeholder={placeholder}
+       step="0.00001"
+       value={value}
+       onChange={(e) => handleChange(e, name)}
+       className='my-2 w-full rounded-md p-2 outline-none bg-trasparent text-white border-none text-sm white-glassmorphism'
+       />
+       
+   );
+   const handleSubmit = () =>{
 
+   }
+   const connectWallet = () =>{
+    
    }
    
    return(
@@ -46,7 +60,40 @@ const Welcome = () => {
            </div>
            <div className="flex flex-1 flex-col items-center justify-center w-full md:mt-0 mt-10 ">
               <div className="p-3 justify-end items-start flex-col rounded-xl h-40 w-full sm:w-72 my-5 eth-card white-glassmorpism ">
-
+                 <div className="flex justify-between flex-col w-full h-full">
+                    <div className="flex justify-between items-start">
+                       <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                          <SiEthereum fontSize={21} color='fff'/>
+                       </div>
+                          <BsInfoCircle fontSize={17} color='fff' />
+                    </div>
+                    <div>
+                        <p className="text-white font-light text-sm">
+                           Address
+                        </p>
+                        <p className="text-white font-semibold text-lg mt-1">
+                           Ethereum
+                        </p>
+                    </div>
+                 </div>
+              </div>
+              <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                 <Imput placeholder="Addres To" name="addresTo" type="text" handleChange={() => {}} />
+                 <Imput placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}} />
+                 <Imput placeholder="Keyword (Gif)" name="keyword " type="text" handleChange={() => {}} />
+                 <Imput placeholder="Enter Message" name="message " type="text" handleChange={() => {}} />
+                 <hr className="h-[1px] w-full border-gray-400 my-2"/>
+                 {false ?(
+                  <Loader/>
+                 ) : (
+                  <button
+                   type="button"
+                   onClick={handleSubmit}
+                   className= "text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer "
+                  >
+                    Send Now
+                  </button>
+                 )}
               </div>
            </div>
         </div>
