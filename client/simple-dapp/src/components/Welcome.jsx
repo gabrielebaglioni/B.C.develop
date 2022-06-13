dcf44a0b4830177d68fd9cd416f35ec34210ce1b
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-13 12:52:46 +0200

    add button

--- client/simple-dapp/src/components/Welcome.jsx
+++ client/simple-dapp/src/components/Welcome.jsx
@@ -3,6 +3,9 @@ import{ SiEthereum } from "react-icons/si";
 import{ BsInfoCircle } from "react-icons/bs";
 import { Loader } from './'
 const Welcome = () => {
+   const connectWallet = () =>{
+
+   }
    return(
      <div className="w-full flex justify-center items-center ">
         <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -13,6 +16,10 @@ const Welcome = () => {
               <p className="text-left text-white mt-5 font-light md:w-9/12 w-11/12 text-base">
                Explore the crypro world. Buy and sell cryptocurrencies easely on krypt
               </p>
+              <button type="button" onClick={connectWallet}
+                       className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                       Connect Wallet
+              </button>
            </div>
         </div>
      </div>
