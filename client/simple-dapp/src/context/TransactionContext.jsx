6dff8c86d275edb1cbe81dc312d358fe9153d479
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-20 15:57:55 +0200

    my project now can store and trasfer data/money on the goerli test network and the transactions work

--- client/simple-dapp/src/context/TransactionContext.jsx
+++ client/simple-dapp/src/context/TransactionContext.jsx
@@ -13,17 +13,18 @@ const getEthereumContract = () => {
    const provider = new ethers.providers.Web3Provider(ethereum);
    const signer = provider.getSigner();
    const transactionsContract = new ethers.Contract(contractAddress, contractABI, signer);
-   // return transactionsContract;
-  console.log({
-   provider,
-   signer,
-   transactionsContract
-  });
+    return transactionsContract;
+//   console.log({
+//    provider,
+//    signer,
+//    transactionsContract
+//   });
 }
 export const TransactionProvider = ({children}) => {
    const [currentAccount, setCurrentAccount] = useState("");
    const [formData, setFormData] = useState({ addressTo: "", amount: "", keyword: "", message: "" });
-
+   const [isLoading, setIsLoading] = useState(false);
+   const [transactionCount, setTransactionCount] = useState(localStorage.getItem("transactionCount"));
 
    const handleChange = (e, name) => {
       setFormData((prevState) => ({ ...prevState, [name]: e.target.value }));
@@ -73,7 +74,32 @@ export const TransactionProvider = ({children}) => {
 
             const { addressTo, amount, keyword, message } = formData;
 
-            getEthereumContract();
+          const transactionsContract =  getEthereumContract();
+          const parsedAmount = ethers.utils.parseEther(amount);
+
+          await ethereum.request({
+            method: "eth_sendTransaction",
+            params: [{
+               from: currentAccount,
+               to: addressTo,
+               gas: '0x5208', // 21000 gwei 
+               value: parsedAmount._hex,  // we have to convert the decimal number from the form in to a gwei number to be accettabile
+            }]
+          });
+ 
+          const transactionHash = await transactionsContract.addToBlockchain( addressTo, parsedAmount, message, keyword );   
+          
+          setIsLoading(true);
+          console.log(`Loading - ${transactionHash.hash}`);
+          await transactionHash.wait();
+          console.log(`Success - ${transactionHash.hash}`);
+          setIsLoading(false);
+
+          const transactionsCount = await transactionsContract.getTransactionCount();
+
+          setTransactionCount(transactionsCount.toNumber());
+          window.location.reload();
+
          } catch (error) {
             console.log(error);
             throw new error ('No Ethereum object')
